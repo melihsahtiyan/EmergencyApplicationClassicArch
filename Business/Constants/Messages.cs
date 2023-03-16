d7f2abe8466c42329d36b607e563ca7b44a75d30
Burak Tırman <buraktrman@gmail.com>
2023-03-16 15:06:13 +0300

    16.03.2023

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+    }
+}
