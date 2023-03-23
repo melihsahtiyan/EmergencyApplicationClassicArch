84d06d2773015f1a1ab636bd6c99437b6f7088dc
Edvin Davulcu <edvindavulcu@gmail.com>
2023-03-23 16:59:10 +0300

    Source DTO

---
+++ Entity/Dtos/Source/SourceForCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dtos.Source
+{
+    public class SourceForCreateDto
+    {
+        public int Id { get; set; }
+        public string SourcePath { get; set; }
+        public DateTime Date { get; set; }
+        public int PostId { get; set; }
+        public string SourceCategory { get; set; }
+    }
+}
